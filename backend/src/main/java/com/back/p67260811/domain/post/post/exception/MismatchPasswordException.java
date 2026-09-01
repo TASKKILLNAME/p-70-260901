a08dcbd3f390d5a88187fed655df550a7214775a
202244067 <dongjunjwa4020@naver.com>
2026-09-01 12:22:06 +0900

    UUID로 사용자 검증

---
+++ backend/src/main/java/com/back/p67260811/domain/post/post/exception/MismatchPasswordException.java
@@ -0,0 +1,8 @@
+package com.back.p67260811.domain.post.post.exception;
+
+public class MismatchPasswordException extends RuntimeException{
+
+    public MismatchPasswordException(String str) {
+        super(str);
+    }
+}
